45c3e3704260a8fbda0c6656aed3c882d49a43c1
jessica930808 <a0905591780@gmail.com>
2023-11-13 19:05:44 +0800

    v0

---
+++ introduce.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>自我介紹</title>
+
+	<style type="text/css">
+		* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+		h1 {color:#000080; font-size:40px;}
+		h2 {color:#000080; font-size:30px;}
+		h3 {color:#000080;font-size:25px;}
+		body{
+			background-color:rgb(226, 226, 226);
+		}
+		table td {
+ 			vertical-align:top;	
+		}
+		
+	</style>
+
+</head>
+<body>
+	 
+	<table width="70%" >
+	<tr>
+		<tr>
+			<td align="center"><h1>吳雨宙(YU-ZHOU WU)</h1></td>
+		</tr>
+
+		<tr>
+			<td align="center"><h2>靜宜大學 資訊管理學系</h2></td>
+		</tr>
+
+		<tr>
+			<td align="center"><img src="/static/1.jpg" alt="照片" width="200" id="picture"></img></td>
+		</tr>
+	</tr>
+	</table>
+	
+	<table width="70%" border="1" >
+	<tr>
+		<td align="center"><h3>關於我</h3>系級：資訊管理學系二A<br>
+		學號: 411134337<br>
+		E-Mail:<a href="mailto:s1113433@o365st.pu.edu.tw">s1113433@o365st.pu.edu.tw</a><br></td>
+			
+		
+			
+		
+		<td align="center"><h3>自我介紹</h3>我是吳雨宙，目前就讀資訊管理學系二年級，未來想往MIS工程師的方向前進，而我現在自認程式能力不足，想要在剩下的兩年半內利用網路上的免費課程及資訊加強程式能力並考取一些有用的證照。</td>
+		
+		
+
+		<td align="center"><h3>推薦影片</h3><iframe src="https://www.youtube.com/embed/s6yEShTa1P0" allowfullscreen></iframe></td>	
+	</tr>
+
+</table>	
+	<?php echo date("2023-11-15")?>
+	Copyright © 吳雨宙. All Rights Reserved.
+</body>
+</html>
